075a2edad1b26a0fbf1f7c7c315307a38e4cc654
Sighvatur Sveinsson <sighvatursv373@nemi.tskoli.is>
2017-01-25 14:30:46 +0000

    opna skranna, skrifa 3 linur og loka

--- gso_git.py
+++ gso_git.py
@@ -6,8 +6,15 @@
 skra = input("Hvað á skráin að heita ")
 skraNafn = skra + ".txt"
 
-minSkra = open(skraNafn, "w+")
+minSkra = open(skraNafn, "w")
 
 #Skrifa í skránna og loka henni
-minSkra.write("Ég elska pepsi max")
+minSkra.write("Ég elska pepsi max" + "\n")
+minSkra.close()
+
+#Opna skránna aftur, skrifa amk 3 línur og loka henni
+minSkra = open(skraNafn, "a")
+for i in range(3):
+    lina = input("Skráðu nýja línu í skránna ")
+    minSkra.write(lina + "\n")
 minSkra.close()
